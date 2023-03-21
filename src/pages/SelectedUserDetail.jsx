eb89450c61ae41d6907295d6a3daa2eff938b1ff
Jose Andrés <oliver_ja@outlook.com>
2023-03-21 20:48:39 +0100

    fix: added back button in selectedDetailUser

--- src/pages/SelectedUserDetail.jsx
+++ src/pages/SelectedUserDetail.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { usersData } from '../helpers/usersSlice';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { usersData, addSelected } from '../helpers/usersSlice';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
 export const SelectedUserDetail = () => {
 
     const users = useSelector(usersData);
+    const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    useEffect(()=>{
+        if(users.selected.length === 0){
+            navigate('/medic');
+        }
+    },[users]);
     return (
         <>
-            <Button onClick={()=>navigate(-1)}>Atras</Button>
+            <Button onClick={()=>{
+                dispatch(addSelected({selected:[]}));
+                navigate(-1);
+            }}>Atras</Button>
             <div>{users.selected.name}</div>
         </>
     )
