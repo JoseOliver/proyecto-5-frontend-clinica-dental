0854b2c8c5ad25d308283455880c2c4eaf1e98cf
Jose Andrés <oliver_ja@outlook.com>
2023-04-04 13:29:23 +0200

    feat: persistent store enabled

--- src/app/store.js
+++ src/app/store.js
@@ -4,11 +4,28 @@ import usersSlice from '../helpers/usersSlice';
 import appointmentsSlice from '../helpers/appointmentsSlice';
 import appointmentSlice from '../helpers/appointmentSlice';
 
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+    user: userSlice,
+    users: usersSlice,
+    appointment: appointmentSlice,
+    appointments: appointmentsSlice
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+
 export default configureStore({
-    reducer: {
-        user: userSlice,
-        users: usersSlice,
-        appointment: appointmentSlice,
-        appointments: appointmentsSlice
-    }
+    reducer: persistedReducer,
+    middleware: [thunk]
 });
--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,19 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './app/store';
 
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
